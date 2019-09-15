97b9510d0f91957e135815cb91d8db51be7bcd45
KuryakovDen <fifer2@yandex.ru>
2019-09-15 22:05:53 +0700

    refactor: refacoring for the function renderStatistics()

--- js/stat.js
+++ js/stat.js
@@ -5,12 +5,15 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 80;
 var GAP = 10;
-var START_GAP = 150;
+var START_GAP = 130;
 var FONT_GAP = 170;
-var TEXT_GAP = 90;
+var TEXT_GAP = 100;
 var TEXT_WIDTH = 30;
 var BAR_HEIGHT = 150;
+
 var barWidth = GAP + TEXT_WIDTH;
+var heightY = CLOUD_Y + GAP;
+var textHeight = CLOUD_Y + GAP + FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -26,19 +29,33 @@ window.renderStatistics = function (ctx) {
   ctx.fillText('Ура вы победили!', 130, 50);
   ctx.fillText('Список результатов:', 130, 70);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Вы', START_GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+  /* var players = ['Вы', 'Ваня', 'Аня', 'Петя'];*/
+
+  var playerName = 'Вы';
+  var playerIndex = 0;
 
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Ваня', START_GAP + TEXT_GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillRect((CLOUD_X * 2) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
+  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Аня', START_GAP + (TEXT_GAP * 2), CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillRect((CLOUD_X * 3) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+  playerName = 'Ваня';
+  playerIndex = 1;
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Петя', START_GAP + START_GAP + START_GAP, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillRect((CLOUD_X * 4) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+  ctx.fillStyle = 'hsl(240,100%, ' + (Math.round(Math.random()) * 200) + ')';
+  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
+  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
+
+  playerName = 'Аня';
+  playerIndex = 2;
+
+  ctx.fillStyle = 'hsl(240,100%, ' + Math.random() + ')';
+  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
+  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
+
+  playerName = 'Петя';
+  playerIndex = 3;
+
+  ctx.fillStyle = 'hsl(240,100%, ' + Math.random() + ')';
+  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
+  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
 };
