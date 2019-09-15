ed0c0c6375fc9697979243a776e6210af9935199
KuryakovDen <fifer2@yandex.ru>
2019-09-15 18:53:22 +0700

    feat: added text message on the window

--- js/stat.js
+++ js/stat.js
@@ -2,12 +2,15 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-/* var CLOUD_X = 100;
-var CLOUD_Y = 40;
+var CLOUD_X = 100;
+var CLOUD_Y = 80;
 var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 150;*/
+var START_GAP = 150;
+var FONT_GAP = 170;
+var TEXT_GAP = 90;
+var TEXT_WIDTH = 30;
+var BAR_HEIGHT = 150;
+var barWidth = GAP + TEXT_WIDTH;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -18,19 +21,24 @@ window.renderStatistics = function (ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Вы', 150, 260);
-  ctx.fillRect(135, 90, 40, 150);
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 130, 50);
+  ctx.fillText('Список результатов:', 130, 70);
 
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Ваня', 240, 260);
-  ctx.fillRect(235, 90, 40, 150);
+  ctx.fillText('Вы', START_GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
 
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Аня', 340, 260);
-  ctx.fillRect(335, 90, 40, 150);
+  ctx.fillText('Ваня', START_GAP + TEXT_GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect((CLOUD_X * 2) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
 
   ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText('Петя', 440, 260);
-  ctx.fillRect(435, 90, 40, 150);
+  ctx.fillText('Аня', START_GAP + (TEXT_GAP * 2), CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect((CLOUD_X * 3) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
+
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  ctx.fillText('Петя', START_GAP + START_GAP + START_GAP, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillRect((CLOUD_X * 4) + GAP + TEXT_WIDTH, CLOUD_Y + GAP, barWidth, BAR_HEIGHT);
 };
