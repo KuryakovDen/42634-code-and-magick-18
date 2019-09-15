daabbdb3da83dd86dc47bbf572903742ba43c993
KuryakovDen <fifer2@yandex.ru>
2019-09-15 12:20:09 +0700

    feat: beggining of the creating bar graph

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,19 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.8)');
   renderCloud(ctx, 100, 10, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.fillText('Вы', 150, 260);
+  ctx.fillRect(135, 90, 50, 150);
+
+  ctx.fillText('Ваня', 240, 260);
+  ctx.fillRect(235, 90, 50, 150);
+
+  ctx.fillText('Аня', 340, 260);
+  ctx.fillRect(335, 90, 50, 150);
+
+  ctx.fillText('Петя', 440, 260);
+  ctx.fillRect(435, 90, 50, 150);
 };
