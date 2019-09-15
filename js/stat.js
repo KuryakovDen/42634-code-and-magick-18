fdd8f8a5ee9afb0c39467fc727c0c7d623eca512
KuryakovDen <fifer2@yandex.ru>
2019-09-15 14:09:52 +0700

    feat: creating approximation histogram

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,12 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+/* var CLOUD_X = 100;
+var CLOUD_Y = 40;
+var GAP = 10;
+var FONT_GAP = 15;
+var TEXT_WIDTH = 50;
+var BAR_HEIGHT = 150;*/
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -9,19 +15,22 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 window.renderStatistics = function (ctx) {
-  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.8)');
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.fillText('Вы', 150, 260);
-  ctx.fillRect(135, 90, 50, 150);
+  ctx.fillRect(135, 90, 40, 150);
 
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.fillText('Ваня', 240, 260);
-  ctx.fillRect(235, 90, 50, 150);
+  ctx.fillRect(235, 90, 40, 150);
 
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.fillText('Аня', 340, 260);
-  ctx.fillRect(335, 90, 50, 150);
+  ctx.fillRect(335, 90, 40, 150);
 
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.fillText('Петя', 440, 260);
-  ctx.fillRect(435, 90, 50, 150);
+  ctx.fillRect(435, 90, 40, 150);
 };
