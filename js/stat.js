6daf183e3d3666bec9b68c9ffb688520f7f832f2
KuryakovDen <fifer2@yandex.ru>
2019-09-18 21:39:50 +0700

    feat: colored columns of the histogamma

--- js/stat.js
+++ js/stat.js
@@ -5,7 +5,7 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 80;
 var GAP = 10;
-var START_GAP = 130;
+var START_GAP = 140;
 var FONT_GAP = 170;
 var TEXT_GAP = 100;
 var TEXT_WIDTH = 30;
@@ -24,38 +24,18 @@ window.renderStatistics = function (ctx) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 130, 50);
   ctx.fillText('Список результатов:', 130, 70);
 
-  /* var players = ['Вы', 'Ваня', 'Аня', 'Петя'];*/
+  var players = ['Вы', 'Ваня', 'Аня', 'Кекс'];
 
-  var playerName = 'Вы';
-  var playerIndex = 0;
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(players[i], START_GAP + (TEXT_GAP * i), textHeight);
+    ctx.fillRect(START_GAP + (CLOUD_X * i), heightY, barWidth, BAR_HEIGHT);
 
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
-  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
-
-  playerName = 'Ваня';
-  playerIndex = 1;
-
-  ctx.fillStyle = 'hsl(240,100%, ' + (Math.round(Math.random()) * 200) + ')';
-  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
-  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
-
-  playerName = 'Аня';
-  playerIndex = 2;
-
-  ctx.fillStyle = 'hsl(240,100%, ' + Math.random() + ')';
-  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
-  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
-
-  playerName = 'Петя';
-  playerIndex = 3;
-
-  ctx.fillStyle = 'hsl(240,100%, ' + Math.random() + ')';
-  ctx.fillText(playerName, START_GAP + (TEXT_GAP * playerIndex), textHeight);
-  ctx.fillRect(START_GAP + (CLOUD_X * playerIndex), heightY, barWidth, BAR_HEIGHT);
+    /* ctx.fillStyle = i > 0 ? 'hsl(240,' + 100 * Math.random() + '%' + ', 50%)' : 'rgba(255, 0, 0, 1)';*/
+    ctx.fillStyle = 'hsl(240,' + 100 * Math.random() + '%' + ', 50%)';
+  }
 };
