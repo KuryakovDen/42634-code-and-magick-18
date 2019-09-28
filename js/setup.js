56839b333ff161851f948e5c6d7ad59b7ff54186
KuryakovDen <fifer2@yandex.ru>
2019-09-28 18:45:31 +0700

    feat: create random-value generation function

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,18 @@
 'use strict';
 
-var wizardNames = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф' ,'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomElement  = function (array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  var randomElement = array[randomIndex];
+  return randomElement;
+}
+
+var consoleItem = getRandomElement(names);
+console.log(consoleItem);
 
 var gamePopup = document.querySelector('.setup');
 gamePopup.classList.remove('hidden');
@@ -10,10 +22,10 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarCharacters = document.querySelector('.setup-similar-list');
 var similarWirardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-for (var i = 0; i<wizardNames.length; i++) {
+for (var i = 0; i<randomNames.length; i++) {
   var wizardElement = similarWirardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizardNames[i];
+  wizardElement.querySelector('.setup-similar-label').textContent = randomNames[i];
 
   similarCharacters.appendChild(wizardElement);
 }
