8c17650e7557d1079500f331b3ada57cf8fb8a81
KuryakovDen <fifer2@yandex.ru>
2019-09-28 16:20:18 +0700

    feat: show the window, that contents choice of wizard

---
+++ js/setup.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var gamePopup = document.querySelector('.setup');
+gamePopup.classList.remove('hidden');
+
+var similarCharacters = document.querySelector('.setup-similar-list');
+var similarWirardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
