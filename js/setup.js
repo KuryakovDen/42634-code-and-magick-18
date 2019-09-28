f7d2a731d22db77af5bc844b31a3e9b292d23f75
KuryakovDen <fifer2@yandex.ru>
2019-09-28 16:26:59 +0700

    feat: show block with similar wizards

--- js/setup.js
+++ js/setup.js
@@ -3,5 +3,12 @@
 var gamePopup = document.querySelector('.setup');
 gamePopup.classList.remove('hidden');
 
+document.querySelector('.setup-similar').classList.remove('hidden');
+
 var similarCharacters = document.querySelector('.setup-similar-list');
 var similarWirardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+for (var i = 0; i<4; i++) {
+  var wizardElement = similarWirardTemplate.cloneNode(true);
+  similarCharacters.appendChild(wizardElement);
+}
