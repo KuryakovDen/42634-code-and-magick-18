ca287c8fba80b3828a204f2a1004b44fd8ccb76c
KuryakovDen <fifer2@yandex.ru>
2019-09-28 20:14:44 +0700

    feat: creation function for test array of objects

--- js/setup.js
+++ js/setup.js
@@ -1,43 +1,45 @@
 'use strict';
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф' ,'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomElement  = function (array) {
+var getRandomElement = function (array) {
   var randomIndex = Math.floor(Math.random() * array.length);
   var randomElement = array[randomIndex];
   return randomElement;
-}
+};
 
 var getRandomFullname = function (firstArray, secondArray) {
   var wirardFullname = getRandomElement(firstArray) + ' ' + getRandomElement(secondArray);
   return wirardFullname;
-}
+};
 
-var wirardFullname = getRandomFullname(names, lastnames);
-console.log(wirardFullname);
+var createRandomObjects = function (objectsCount) {
+  var arrayData = [];
 
-/*
-var wizads = [{
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }, {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }, {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }, {
-    name: '',
-    coatColor: '',
-    eyesColor: ''
-  }];
-  */
+  for (var i = 0; i < objectsCount; i++) {
+    var objectData = {
+      name: getRandomFullname(names, lastnames),
+      coatColor: getRandomElement(coatColors),
+      eyesColor: getRandomElement(eyesColors)
+    };
+
+    arrayData[i] = objectData;
+  }
+
+  return arrayData;
+};
+
+var testData = createRandomObjects(4);
+
+/* var wizardName = getRandomElement(names);
+var wizardLastName = getRandomElement(lastnames);
+var wizardCoatColor = getRandomElement(coatColors);
+var wizardEyeColor = getRandomElement(eyesColors);
+var wirardFullname = getRandomFullname(names, lastnames);*/
+
 
 var gamePopup = document.querySelector('.setup');
 gamePopup.classList.remove('hidden');
