5ba828238354dd4f53e2dec0620889df330716ce
KuryakovDen <fifer2@yandex.ru>
2019-09-29 16:44:23 +0700

    refactor: refactoring for the function 'createRandomWizards'

--- js/setup.js
+++ js/setup.js
@@ -1,38 +1,35 @@
 'use strict';
 
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var wizardLastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-
-var wizards = [];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
 
 var getRandomElement = function (array) {
   var randomIndex = Math.floor(Math.random() * array.length);
-  var randomElement = array[randomIndex];
-  return randomElement;
+  return array[randomIndex];
 };
 
-var getRandomFullname = function (firstArray, secondArray) {
-  return getRandomElement(firstArray) + ' ' + getRandomElement(secondArray);
+var getRandomFullname = function (names, lastnames) {
+  return getRandomElement(names) + ' ' + getRandomElement(lastnames);
 };
 
-var createRandomObjects = function (objectsCount) {
-  for (var i = 0; i < objectsCount; i++) {
-    var singleWizard = {
-      name: getRandomFullname(names, lastnames),
+var createRandomWizards = function (wizardsCount) {
+  var randomWizards = [];
+
+  for (var i = 0; i < wizardsCount; i++) {
+    randomWizards.push({
+      name: getRandomFullname(wizardNames, wizardLastnames),
       coatColor: getRandomElement(coatColors),
       eyesColor: getRandomElement(eyesColors)
-    };
-
-    wizards[i] = singleWizard;
+    });
   }
 
-  return wizards;
+  return randomWizards;
 };
 
-createRandomObjects(4);
+var wizards = createRandomWizards(4);
 
 var gamePopup = document.querySelector('.setup');
 gamePopup.classList.remove('hidden');
