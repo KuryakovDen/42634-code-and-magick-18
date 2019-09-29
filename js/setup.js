a4016a09d0bed86f41e7be3ec967f528dcd7be73
KuryakovDen <fifer2@yandex.ru>
2019-09-29 15:17:28 +0700

    fix: variables were renamed + refactoring for name-creating function

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,10 @@
 var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var lastnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+
+var wizards = [];
 var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
 
 var getRandomElement = function (array) {
   var randomIndex = Math.floor(Math.random() * array.length);
@@ -13,28 +16,24 @@ var getRandomElement = function (array) {
 };
 
 var getRandomFullname = function (firstArray, secondArray) {
-  var wirardFullname = getRandomElement(firstArray) + ' ' + getRandomElement(secondArray);
-  return wirardFullname;
+  return getRandomElement(firstArray) + ' ' + getRandomElement(secondArray);
 };
-
-var arrayData = [];
 
 var createRandomObjects = function (objectsCount) {
   for (var i = 0; i < objectsCount; i++) {
-    var objectData = {
+    var singleWizard = {
       name: getRandomFullname(names, lastnames),
       coatColor: getRandomElement(coatColors),
       eyesColor: getRandomElement(eyesColors)
     };
 
-    arrayData[i] = objectData;
+    wizards[i] = singleWizard;
   }
 
-  return arrayData;
+  return wizards;
 };
 
-var testData = createRandomObjects(4);
-console.log(testData);
+createRandomObjects(4);
 
 var gamePopup = document.querySelector('.setup');
 gamePopup.classList.remove('hidden');
@@ -44,12 +43,12 @@ document.querySelector('.setup-similar').classList.remove('hidden');
 var similarCharacters = document.querySelector('.setup-similar-list');
 var similarWirardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-for (var i = 0; i < arrayData.length; i++) {
+for (var i = 0; i < wizards.length; i++) {
   var wizardElement = similarWirardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = arrayData[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = arrayData[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = arrayData[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
 
   similarCharacters.appendChild(wizardElement);
 }
