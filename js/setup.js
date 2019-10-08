04d260e4cf3be7d8b1ca2fdbd8a55c0c638dc357
KuryakovDen <fifer2@yandex.ru>
2019-10-08 22:08:55 +0700

    refactor: created some functions-handler for printing error message

--- js/setup.js
+++ js/setup.js
@@ -110,26 +110,34 @@ var closePopup = function () {
   modalWindow.classList.add('hidden');
 };
 
-getSetupUserName().addEventListener('invalid', function () {
-  if (getSetupUserName().validity.tooShort) {
-    getSetupUserName().setCustomValidity('Имя персонажа не должно быть короче 2 символов');
-  } else if (getSetupUserName().validity.tooLong) {
-    getSetupUserName().setCustomValidity('Имя персонажа не должно быть длиннее 25 символов');
-  } else if (getSetupUserName().validity.valueMissing) {
-    getSetupUserName().setCustomValidity('Это поле обязательно для заполнения!');
-  } else {
-    getSetupUserName().setCustomValidity('');
-  }
-});
+var inputValidityHandler = function (validityParam) {
+  validityParam = getSetupUserName().validity;
+  var message = '';
 
-getSetupUserName().addEventListener('input', function (evt) {
+  if (validityParam.tooShort) {
+    message = 'Имя персонажа не должно быть короче 2 символов!';
+  } else if (validityParam.tooLong) {
+    message = 'Имя персонажа не должно быть длиннее 25 символов!';
+  } else if (validityParam.valueMissing) {
+    message = 'Это поле обязательно для заполнения!';
+  } else {
+    message = '';
+  }
+
+  return getSetupUserName().setCustomValidity(message);
+};
+
+var inputSmallLengthValidityHandler = function (evt) {
   var target = evt.target;
   if (target.value.length < 2) {
-    target.setCustomValidity('Имя персонажа не должно быть короче 2 символов');
+    target.setCustomValidity('Имя персонажа не должно быть короче 2 символов!');
   } else {
     target.setCustomValidity('');
   }
-});
+};
+
+getSetupUserName().addEventListener('invalid', inputValidityHandler);
+getSetupUserName().addEventListener('input', inputSmallLengthValidityHandler);
 
 getSetupOpenWindow().addEventListener('click', function () {
   openPopup();
